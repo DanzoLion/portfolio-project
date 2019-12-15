82cdb1ddddcc0a3f278ed43d8fdbab3a9cf0bd86
Daniel <maildanielrae@gmail.com>
2019-12-15 06:43:32 +0000

    Added bootstrap framework with jobs app

--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,9 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
